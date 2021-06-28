51511c0c0ccd63e6fc01f2b008ba5b9505867cde
Dominik Sander <dominik.sander@googlemail.com>
2021-06-28 22:03:00 +0200

    add user profile

---
+++ components/layout/nav/ProfileDropdown.vue
@@ -0,0 +1,169 @@
+<template>
+  <!-- Profile dropdown -->
+  <div class="ml-3 relative" v-click-outside="hide">
+    <div>
+      <button
+        @click="profileOpen ? hide() : show()"
+        id="user-menu-button"
+        type="button"
+        class="
+          max-w-xs
+          bg-white
+          flex
+          items-center
+          text-sm
+          rounded-full
+          focus:outline-none
+          focus:ring-2
+          focus:ring-offset-2
+          focus:ring-indigo-500
+        "
+        aria-expanded="false"
+        aria-haspopup="true"
+      >
+        <span class="sr-only">Open user menu</span>
+        <img
+          class="h-8 w-8 rounded-full"
+          src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+          alt=""
+        />
+      </button>
+    </div>
+
+    <transition
+      enter-active-class="transition ease-out duration-100"
+      leave-active-class="transition ease-in duration-75"
+      enter-class="transform opacity-0 scale-95"
+      enter-to-class="transform opacity-100 scale-100"
+      leave-class="transform opacity-100 scale-100"
+      leave-to-class="transform opacity-0 scale-95"
+    >
+      <div
+        v-if="profileOpen"
+        class="
+          origin-top-right
+          absolute
+          right-0
+          mt-2
+          w-48
+          rounded-md
+          divide-y divide-gray-200
+          shadow-lg
+          py-1
+          bg-white
+          ring-1 ring-black ring-opacity-5
+          focus:outline-none
+        "
+        role="menu"
+        aria-orientation="vertical"
+        aria-labelledby="user-menu-button"
+        tabindex="-1"
+      >
+        <div class="py-1" role="none">
+          <!-- Active: "bg-gray-100", Not Active: "" -->
+          <a
+            id="user-menu-item-0"
+            href="#"
+            class="block px-4 py-2 text-sm text-gray-700"
+            role="menuitem"
+            tabindex="-1"
+            >Your Profile</a
+          >
+
+          <a
+            id="user-menu-item-1"
+            href="#"
+            class="block px-4 py-2 text-sm text-gray-700"
+            role="menuitem"
+            tabindex="-1"
+            >Settings</a
+          >
+
+          <a
+            id="user-menu-item-2"
+            href="#"
+            class="block px-4 py-2 text-sm text-gray-700"
+            role="menuitem"
+            tabindex="-1"
+            >Sign out</a
+          >
+        </div>
+        <div class="py-1" role="none">
+          <div
+            class="flex items-center px-4 py-2 cursor-pointer"
+            @click="colormode === 'dark' ? disableDarkmode() : enableDarkmode()"
+          >
+            <button
+              id="darkmode"
+              type="button"
+              class="
+                relative
+                inline-flex
+                flex-shrink-0
+                h-6
+                w-11
+                border-2 border-transparent
+                rounded-full
+                transition-colors
+                ease-in-out
+                duration-200
+                focus:outline-none
+                focus:ring-2
+                focus:ring-offset-2
+                focus:ring-indigo-500
+              "
+              :class="colormode === 'dark' ? 'bg-primary-700' : 'bg-gray-200'"
+              role="switch"
+            >
+              <span
+                aria-hidden="true"
+                class="
+                  pointer-events-none
+                  inline-block
+                  h-5
+                  w-5
+                  rounded-full
+                  bg-white
+                  shadow
+                  transform
+                  ring-0
+                  transition
+                  ease-in-out
+                  duration-200
+                "
+                :class="
+                  colormode === 'dark' ? 'translate-x-5' : 'translate-x-0'
+                "
+              ></span>
+            </button>
+            <span id="darkmode-label" class="ml-3">
+              <span class="text-sm text-gray-900">Darkmode</span>
+            </span>
+          </div>
+        </div>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+import { mapMutations, mapState } from 'vuex'
+
+export default {
+  name: 'ProfileDropdown',
+  computed: {
+    ...mapState({
+      profileOpen: (state) => state.layout.profileOpen,
+      colormode: (state) => state.layout.colormode
+    })
+  },
+  methods: {
+    ...mapMutations({
+      show: 'layout/openProfile',
+      hide: 'layout/closeProfile',
+      enableDarkmode: 'layout/enableDarkmode',
+      disableDarkmode: 'layout/disableDarkmode'
+    })
+  }
+}
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -48,152 +48,7 @@
             <layout-logo class="h-6 m-auto mx-auto my-5 md:hidden" />
           </div>
           <div class="ml-4 flex items-center md:ml-6">
-            <!-- Profile dropdown -->
-            <div class="ml-3 relative">
-              <div>
-                <button
-                  id="user-menu-button"
-                  type="button"
-                  class="
-                    max-w-xs
-                    bg-white
-                    flex
-                    items-center
-                    text-sm
-                    rounded-full
-                    focus:outline-none
-                    focus:ring-2
-                    focus:ring-offset-2
-                    focus:ring-indigo-500
-                  "
-                  aria-expanded="false"
-                  aria-haspopup="true"
-                >
-                  <span class="sr-only">Open user menu</span>
-                  <img
-                    class="h-8 w-8 rounded-full"
-                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                    alt=""
-                  />
-                </button>
-              </div>
-
-              <!--
-              Dropdown menu, show/hide based on menu state.
-
-              Entering: "transition ease-out duration-100"
-                From: "transform opacity-0 scale-95"
-                To: "transform opacity-100 scale-100"
-              Leaving: "transition ease-in duration-75"
-                From: "transform opacity-100 scale-100"
-                To: "transform opacity-0 scale-95"
-            -->
-              <div
-                class="
-                  origin-top-right
-                  absolute
-                  right-0
-                  mt-2
-                  w-48
-                  rounded-md
-                  shadow-lg
-                  py-1
-                  bg-white
-                  ring-1 ring-black ring-opacity-5
-                  focus:outline-none
-                "
-                role="menu"
-                aria-orientation="vertical"
-                aria-labelledby="user-menu-button"
-                tabindex="-1"
-              >
-                <!-- Active: "bg-gray-100", Not Active: "" -->
-                <a
-                  id="user-menu-item-0"
-                  href="#"
-                  class="block px-4 py-2 text-sm text-gray-700"
-                  role="menuitem"
-                  tabindex="-1"
-                  >Your Profile</a
-                >
-
-                <a
-                  id="user-menu-item-1"
-                  href="#"
-                  class="block px-4 py-2 text-sm text-gray-700"
-                  role="menuitem"
-                  tabindex="-1"
-                  >Settings</a
-                >
-
-                <a
-                  id="user-menu-item-2"
-                  href="#"
-                  class="block px-4 py-2 text-sm text-gray-700"
-                  role="menuitem"
-                  tabindex="-1"
-                  >Sign out</a
-                >
-                <div
-                  class="flex items-center px-4 py-2"
-                  @click="toggeleDarkMode"
-                >
-                  <button
-                    id="darkmode"
-                    type="button"
-                    class="
-                      relative
-                      inline-flex
-                      flex-shrink-0
-                      h-6
-                      w-11
-                      border-2 border-transparent
-                      rounded-full
-                      cursor-pointer
-                      transition-colors
-                      ease-in-out
-                      duration-200
-                      focus:outline-none
-                      focus:ring-2
-                      focus:ring-offset-2
-                      focus:ring-indigo-500
-                    "
-                    :class="
-                      $colorMode.preference === 'dark'
-                        ? 'bg-primary-700'
-                        : 'bg-gray-200'
-                    "
-                    role="switch"
-                  >
-                    <span
-                      aria-hidden="true"
-                      class="
-                        pointer-events-none
-                        inline-block
-                        h-5
-                        w-5
-                        rounded-full
-                        bg-white
-                        shadow
-                        transform
-                        ring-0
-                        transition
-                        ease-in-out
-                        duration-200
-                      "
-                      :class="
-                        $colorMode.preference === 'dark'
-                          ? 'translate-x-5'
-                          : 'translate-x-0'
-                      "
-                    ></span>
-                  </button>
-                  <span id="darkmode-label" class="ml-3">
-                    <span class="text-sm text-gray-900">Darkmode</span>
-                  </span>
-                </div>
-              </div>
-            </div>
+            <LayoutNavProfileDropdown />
           </div>
         </div>
       </div>
@@ -208,10 +63,11 @@
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 export default {
   data() {
     return {
-      userMenuOpen: false,
       openNav: false,
       active: 'Liga',
       nav: [
@@ -222,8 +78,8 @@ export default {
         'Spieler',
         'Office',
         'Lines',
-        'Forum',
-      ],
+        'Forum'
+      ]
     }
   },
   computed: {
@@ -233,24 +89,22 @@ export default {
           /* webpackChunkName: `icon/[request]` */ `~/static/logo-white-small.svg?inline`
         )
     },
+    ...mapState({
+      colormode: (state) => state.layout.colormode
+    })
+  },
+  watch: {
+    colormode(newMode) {
+      this.$colorMode.preference = newMode
+    }
   },
   methods: {
-    toggleUserNav() {
-      this.userMenuOpen = !this.userMenuOpen
-    },
     showNav() {
       this.openNav = true
     },
     hideNav() {
       this.openNav = false
-    },
-    closeUserNav() {
-      this.userMenuOpen = false
-    },
-    toggeleDarkMode() {
-      this.$colorMode.preference =
-        this.$colorMode.preference === 'light' ? 'dark' : 'light'
-    },
-  },
+    }
+  }
 }
 </script>
