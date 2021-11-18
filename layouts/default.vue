f8984e99b948948a2a0628ca4c1be6b16297ab21
Dominik Sander <dominik.sander@googlemail.com>
2021-11-18 22:33:11 +0100

    add seasonsorter

--- layouts/default.vue
+++ layouts/default.vue
@@ -80,6 +80,7 @@
 <script>
 import gql from 'graphql-tag'
 import { mapState } from 'vuex'
+import seasonSorter from '~/lib/seasonSorter'
 
 export default {
   data() {
@@ -109,26 +110,7 @@ export default {
         }
       `,
       update: ({ seasons }) => {
-        this.$store.commit(
-          'navigation/setAvailableSeasons',
-          [...seasons].sort((a, b) => {
-            const numberA = a.substr(4, 2)
-            const numberB = b.substr(4, 2)
-            if (numberA !== numberB) {
-              if (numberA > numberB) return 1
-              if (numberA < numberB) return -1
-              return 0
-            } else {
-              if (a.includes('pre')) return -1
-              if (b.includes('pre')) return 1
-              if (a.length === 6) return -1
-              if (b.length === 6) return 1
-              if (a.includes('PLF')) return -1
-              if (b.includes('PLF')) return 1
-              return 0
-            }
-          })
-        )
+        this.$store.commit('navigation/setAvailableSeasons', [...seasons].sort(seasonSorter))
       }
     })
   },
