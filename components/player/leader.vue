09e20735db848fe4e068bfc96d2fdf58bbc9b45e
Dominik Sander <dominik.sander@googlemail.com>
2021-12-06 21:09:46 +0100

    update leaders

--- components/player/leader.vue
+++ components/player/leader.vue
@@ -1,71 +1,72 @@
 <template>
-  <div
-    v-if="leader"
-    class="
-      col-span-1
-      flex flex-col
-      text-center
-      bg-white
-      dark:bg-gray-600
-      rounded-lg
-      shadow
-      divide-y divide-gray-600
-      dark:divide-white
-      divide-opacity-20 divide-dashed
-      overflow-hidden
-    "
-  >
-    <div class="flex-1 flex flex-col pt-4 pr-4 pb-1">
-      <div class="flex w-full items-center flex-row pb-3">
-        <div
-          class="
-            flex-grow
-            text-left
-            leading-3
-            p-2
-            overflow-hidden overflow-ellipsis
-            text-primary-700
-            dark:text-white
-          "
-        >
-          <span class="first text-sm">{{ leader.player.display_fname }}</span
-          ><br />
-          <span class="font-bold text-xl uppercase whitespace-nowrap">
-            {{ leader.player.display_lname }}</span
-          >
-        </div>
-        <div
-          class="
-            flex-none
-            w-20
-            min-w-20
-            h-20
-            p-2
-            rounded-full
-            bg-white
-            border-2 border-gray-100
-            shadow
-            bg-no-repeat bg-cover
-            relative
-          "
-          :style="`
-          background-image: url(https://my-tohl.org/img/player/${leader.player.id}.jpg);
-          border-color: ${backgroundColor(leader.team.teamid)};
-          `"
-        >
-          <team-logo-middle
-            :teamid="leader.team.teamid"
-            class="transform scale-50 min-w-20 absolute -bottom-1/2 -right-1/2"
+  <div class="flex flex-col items-end w-full h-36" v-if="leader">
+    <div
+      class="
+        shadow-lg
+        relative
+        isolate
+        overflow-hidden
+        rounded-md
+        w-full
+        h-full
+        border-t-4
+        bg-white
+        dark:bg-gray-700
+      "
+      :style="`border-color: ${leader.team.background}`"
+    >
+      <div class="mx-auto p-1 sm:p-2 flex flex-col sm:flex-row">
+        <div class="w-24">
+          <div
+            class="
+              w-20
+              h-20
+              relative
+              mx-auto
+              rounded-full
+              shadow
+              bg-top bg-cover
+              border-4 border-white
+              bg-white
+            "
+            :style="`background-image: url(https://my-tohl.org/img/player/${leader.player.id}.jpg)`"
           />
+          <team-logo-big :teamid="leader.team.teamid" class="w-12 h-12 relative -mt-5" />
         </div>
-      </div>
-    </div>
-    <div>
-      <div class="flex">
-        <div class="w-0 flex-1 flex-col flex items-center">
-          <span class="font-medium uppercase font-headline text-lg">{{
-            $tc(`leader.${sortby}`, leader[sortby])
-          }}</span>
+        <div
+          class="flex-grow sm:pl-4 flex flex-col pt-2 sm:place-items-start place-items-center gap-1"
+        >
+          <h2
+            class="
+              font-bold font-college
+              text-center
+              inline-block
+              px-2
+              bg-gray-50 bg-opacity-40
+              overflow-ellipsis overflow-hidden
+              whitespace-nowrap
+            "
+          >
+            <span class="text-lg">{{
+              leader.player.display_lname.length + leader.player.display_fname.length > 18
+                ? leader.player.display_fname[0] + '.'
+                : leader.player.display_fname
+            }}</span>
+            <span class="uppercase text-xl">&nbsp;{{ leader.player.display_lname }}</span>
+          </h2>
+          <p
+            class="
+              uppercase
+              text-3xl
+              font-bold font-headline
+              text-center
+              inline-block
+              px-2
+              bg-gray-50 bg-opacity-40
+            "
+          >
+            {{ $tc(`leader.${sortby}`, leader[sortby]) }}
+          </p>
         </div>
       </div>
     </div>
@@ -89,6 +90,8 @@ export default {
             team {
               teamsim
               teamid
+              foreground @client(always: true)
+              background @client(always: true)
             }
             player {
               id
@@ -113,12 +116,14 @@ export default {
         }
       `,
       variables() {
+        console.log('where', this.where)
         return {
           order: { column: this.sortby, order: this.sortby === 'gaa' ? 'ASC' : 'DESC' },
           where: JSON.stringify(this.where)
         }
       },
       update: ({ playerstats }) => {
+        console.log(playerstats)
         if (playerstats.length === 1) {
           return playerstats[0]
         }
