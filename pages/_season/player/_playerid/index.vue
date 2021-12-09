27d7a94fdc1cc063e0ece183a2fc4f1fab6ab357
Dominik Sander <dominik.sander@googlemail.com>
2021-12-09 23:12:50 +0100

    improve stats display

--- pages/_season/player/_playerid/index.vue
+++ pages/_season/player/_playerid/index.vue
@@ -1,116 +1,67 @@
 <template>
   <div class="statstable">
-    <h3 class="font-college font-bold text-xl mx-auto">Career</h3>
-    <table class="mx-auto hidden md:table">
+    <h3 class="font-college font-bold text-xl mx-auto">Regular Season</h3>
+    <table>
       <thead>
         <tr>
-          <th v-for="col of [...cols['*'], ...cols['reg'], 'post', ...cols['plf']]" :key="col">
-            {{ $t(`abbr.${col.split('.')[1] || col}`) }}
-          </th>
+          <th colspan="2"></th>
+          <th></th>
+          <th colspan="3">Scoring</th>
+          <th></th>
+          <th></th>
+          <th colspan="4">Goals</th>
+          <th colspan="3">Assists</th>
+          <th colspan="2">Shots</th>
+          <th colspan="2">Ice Time</th>
+          <th></th>
+        </tr>
+        <tr>
+          <th>Saison</th>
+          <th>Team</th>
+          <th>GP</th>
+          <th>G</th>
+          <th>A</th>
+          <th>PTS</th>
+          <th>+/-</th>
+          <th>PIM</th>
+          <th>EV</th>
+          <th>PP</th>
+          <th>SH</th>
+          <th>GW</th>
+          <th>EV</th>
+          <th>PP</th>
+          <th>SH</th>
+          <th>S</th>
+          <th>S%</th>
+          <th>TOI</th>
+          <th>ATOI</th>
+          <th>HITS</th>
         </tr>
       </thead>
       <tbody v-if="seasons">
-        <tr v-for="(stat, name, index) of seasons" :key="name">
+        <tr v-for="(stat, name, index) of seasons['reg']" :key="name">
           <td
-            v-for="col of [...cols['*'], ...cols['reg'], 'post', ...cols['plf']]"
-            :key="`${stat.reg.season}${stat.reg.team.teamid}${col}`"
+            v-for="col of cols"
+            :key="`${stat.season}${stat.team.teamid}${col}`"
             :class="['post', 'team'].indexOf(col) >= 0 ? '!text-left' : ''"
           >
             <span v-if="col === 'season'">{{
-              !Object.keys(seasons)[index - 1] ||
-              (Object.keys(seasons)[index - 1] &&
-                Object.keys(seasons)[index - 1].substr(0, 6) !== name.substr(0, 6))
+              !Object.keys(seasons['reg'])[index - 1] ||
+              (Object.keys(seasons['reg'])[index - 1] &&
+                Object.keys(seasons['reg'])[index - 1].substr(0, 6) !== name.substr(0, 6))
                 ? name.substr(0, 6)
                 : ''
             }}</span>
-            <span v-else-if="col === 'team'">
-              <team-logo-inline :teamid="stat.reg.team.teamid" /> {{ stat.reg.team.teamsim }}
+            <span v-else-if="col === 'team'" class="uppercase">
+              <team-logo-inline :teamid="stat.team.teamid" /> {{ stat.team.teamid }}
             </span>
-            <span v-else-if="col === 'post'">|</span>
+            <span v-else-if="col === 'spercentage'">{{ shotpercentage(stat) }}%</span>
+            <span v-else-if="col === 'averageicetime'">{{ averageicetime(stat) }}</span>
             <span v-else>{{ get(stat, col, '&mdash;') }}</span>
           </td>
         </tr>
       </tbody>
     </table>
-    <div class="md:hidden w-full">
-      <div class="flex gap-2">
-        <div>Regular Season</div>
-        <button
-          @click="showReg = !showReg"
-          type="button"
-          class="
-            bg-gray-200
-            relative
-            inline-flex
-            flex-shrink-0
-            h-6
-            w-11
-            border-2 border-transparent
-            rounded-full
-            cursor-pointer
-            transition-colors
-            ease-in-out
-            duration-200
-            focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500
-          "
-          role="switch"
-          aria-checked="false"
-        >
-          <span class="sr-only">Playoff switch</span>
-          <!-- Enabled: "translate-x-5", Not Enabled: "translate-x-0" -->
-          <span
-            aria-hidden="true"
-            class="
-              pointer-events-none
-              inline-block
-              h-5
-              w-5
-              rounded-full
-              bg-primary-500
-              shadow
-              transform
-              ring-0
-              transition
-              ease-in-out
-              duration-200
-            "
-            :class="showReg ? 'translate-x-0' : 'translate-x-5'"
-          ></span>
-        </button>
-        <div>Playoffs</div>
-      </div>
-      <table class="w-full">
-        <thead>
-          <tr>
-            <th v-for="col of [...cols['*'], ...cols[showReg ? 'reg' : 'plf']]" :key="col">
-              {{ $t(`abbr.${col.split('.')[1] || col}`) }}
-            </th>
-          </tr>
-        </thead>
-        <tbody v-if="seasons">
-          <tr v-for="(stat, name, index) of seasons" :key="name">
-            <td
-              v-for="col of [...cols['*'], ...cols[showReg ? 'reg' : 'plf']]"
-              :key="`${stat.reg.season}${stat.reg.team.teamid}${col}`"
-              :class="['post', 'team'].indexOf(col) >= 0 ? '!text-left' : ''"
-            >
-              <span v-if="col === 'season'">{{
-                !Object.keys(seasons)[index - 1] ||
-                (Object.keys(seasons)[index - 1] &&
-                  Object.keys(seasons)[index - 1].substr(0, 6) !== name.substr(0, 6))
-                  ? name.substr(0, 6)
-                  : ''
-              }}</span>
-              <span v-else-if="col === 'team'" class="uppercase">
-                {{ stat.reg.team.teamid }}
-              </span>
-              <span v-else-if="col === 'post'">|</span>
-              <span v-else>{{ get(stat, col, '&mdash;') }}</span>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
   </div>
 </template>
 
@@ -127,16 +78,33 @@ export default {
     return {
       showReg: true,
       seasons: {},
-      cols: {
-        '*': ['season', 'team'],
-        reg: ['reg.games', 'reg.goals', 'reg.assists', 'reg.points', 'reg.pim', 'reg.plusminus'],
-        plf: ['plf.games', 'plf.goals', 'plf.assists', 'plf.points', 'plf.pim', 'plf.plusminus']
-      }
+      cols: [
+        'season',
+        'team',
+        'games',
+        'goals',
+        'assists',
+        'points',
+        'plusminus',
+        'pim',
+        'evg',
+        'ppg',
+        'shg',
+        'gwg',
+        'eva',
+        'ppa',
+        'sha',
+        'shots',
+        'spercentage',
+        'icetime',
+        'averageicetime',
+        'hits'
+      ]
     }
   },
   watch: {
     playerstats(newPlayerstats) {
-      const seasons = {}
+      const seasons = { plf: {}, reg: {}, pre: {} }
       // const seasons = [...newPlayerstats.map((p) => p.season)].sort(seasonSorter)
       newPlayerstats.forEach((playerstat) => {
         const s = playerstat.season.substr(0, 6)
@@ -144,11 +112,11 @@ export default {
           seasons[s + playerstat.team.teamid] = {}
         }
         if (playerstat.season.indexOf('PLF') >= 0) {
-          seasons[s + playerstat.team.teamid]['plf'] = playerstat
+          seasons['plf'][s + playerstat.team.teamid] = playerstat
         } else if (playerstat.season.indexOf('pre') >= 0) {
-          seasons[s + playerstat.team.teamid]['pre'] = playerstat
+          seasons['pre'][s + playerstat.team.teamid] = playerstat
         } else {
-          seasons[s + playerstat.team.teamid]['reg'] = playerstat
+          seasons['reg'][s + playerstat.team.teamid] = playerstat
         }
       })
       this.seasons = seasons
@@ -157,6 +125,20 @@ export default {
   methods: {
     get(object, path, defaultValue) {
       return get(object, path, defaultValue)
+    },
+    shotpercentage(stat) {
+      let sp = 0
+      if (stat.goals > 0) {
+        sp = (100 * stat.goals) / stat.shots
+      }
+      return sp.toLocaleString(undefined, { minimumFractionDigits: 1, maximumFractionDigits: 1 })
+    },
+    averageicetime(stat) {
+      let atoi = 0
+      if (stat.icetime > 0) {
+        atoi = stat.icetime / stat.games
+      }
+      return `${Math.floor(atoi)}:${Math.floor((atoi % 1) * 60)}`
     }
   },
   apollo: {
@@ -178,6 +160,8 @@ export default {
             shots
             hits
             icetime
+            evg
+            eva
             ppg
             ppa
             shg
@@ -222,13 +206,13 @@ export default {
 .statstable td,
 .statstable th {
   @apply font-headline;
-  @apply py-2;
-  @apply px-2;
+  @apply py-1 px-2;
+  @apply border border-primary-500;
 }
 @media (min-width: 640px) {
   .statstable td,
   .statstable th {
-    @apply px-3;
+    @apply px-2;
   }
 }
 .statstable th {
