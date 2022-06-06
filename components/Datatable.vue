c7aca92300fd4085aaede1d2b2baa1ed3e965d71
Dominik Sander <dominik.sander@googlemail.com>
2022-06-06 21:17:39 +0200

    selectable table settings

--- components/Datatable.vue
+++ components/Datatable.vue
@@ -6,32 +6,33 @@ const datatable = ref(null)
 
 const props = defineProps({
   tabledata: Array,
-  columns: Array
+  columns: Array,
+  options: Object
 })
-const { tabledata, columns } = toRefs(props)
+const { tabledata, columns, options } = toRefs(props)
 
 watchEffect(() => {
   if (datatable.value) {
     const columns2 = columns.value.map((c) => Object.assign({}, c))
+    const datatableOptions = Object.assign(
+      {
+        data: structuredClone(tabledata.value),
+        columns: columns2,
+        headerSortElement: '',
+        layout: 'fitColumns',
+        groupToggleElement: 'header',
+        height: '90vh',
+        responsiveLayout: true,
+        resizableColumnFit: true
+      },
+      options.value || {}
+    )
 
-    var table = new Tabulator(datatable.value, {
-      data: structuredClone(tabledata.value),
-      columns: columns2,
-      groupBy: 'seasondata.pos',
-      headerSortElement: '',
-      layout: 'fitColumns',
-      groupToggleElement: 'header',
-      height: '90vh',
-      responsiveLayout: true,
-      resizableColumnFit: true,
-      groupHeader: function (value, count) {
-        return value + "<span style='margin-left:10px;'>(" + count + ' Spieler)</span>'
-      }
-    })
+    var table = new Tabulator(datatable.value, datatableOptions)
   }
 })
 </script>
 
 <template>
-  <div ref="datatable" class="table-borderless font-mono"></div>
+  <div ref="datatable" class="table-borderless font-mono mt-3"></div>
 </template>
--- pages/TOHL[season]/team/[team]/Roster.vue
+++ pages/TOHL[season]/team/[team]/Roster.vue
@@ -1,7 +1,9 @@
 <script setup>
-import Datatable from '@/components/Datatable.vue'
-import { computed } from 'vue'
+import { Popover, PopoverButton, PopoverPanel } from '@headlessui/vue'
+import { ChevronDownIcon, CogIcon } from '@heroicons/vue/solid'
+import { computed, ref, watch } from 'vue'
 import { useQuery } from '@vue/apollo-composable'
+import Datatable from '@/components/Datatable.vue'
 import gql from 'graphql-tag'
 
 const route = useRoute()
@@ -89,8 +91,8 @@ const columns = [
     title: 'Pos',
     field: 'seasondata.pos',
     sorter: 'string',
-    width: 27,
-    minWidth: 27,
+    width: 37,
+    minWidth: 37,
     resizable: false
   },
   {
@@ -282,15 +284,117 @@ const columns = [
       symbol: '$',
       precision: false
     }
+  },
+  {
+    title: '#',
+    field: 'seasondata.roster',
+    visible: false,
+    sorter: 'number',
+    hozAlign: 'right',
+    cssClass: 'text-secondary-500 font-bold',
+    width: 27,
+    minWidth: 27,
+    resizable: false,
+    responsive: 2
   }
 ]
+
+const grouping = ref(false)
+const open = ref(false)
+
+const datatableOptions = ref({})
+
+const groups = reactive([
+  { id: 'seasondata.cd', name: 'Fitness', active: ref(false) },
+  { id: 'seasondata.roster', name: 'Zuordnung', active: ref(false) },
+  { id: 'seasondata.pos', name: 'nach Positionen', active: ref(false) }
+])
+
+watch(
+  groups,
+  (newGrouping) => {
+    const groupBy = []
+    for (const group of newGrouping) {
+      if (group.active === true) {
+        groupBy.push(group.id)
+      }
+    }
+    console.log(groupBy)
+    datatableOptions.value = {
+      groupBy,
+      groupHeader: function (value, count) {
+        return value + "<span style='margin-left:10px;'>(" + count + ' Spieler)</span>'
+      }
+    }
+  },
+  { immediate: true }
+)
 </script>
 <template>
   <div class="mt-3">
     <div v-if="loading">laden...</div>
     <div v-else-if="error">oops {{ error }}</div>
     <div v-else>
-      <Datatable :tabledata="players" :columns="columns" />
+      <Popover class="z-10 relative" v-slot="{ open }">
+        <div class="max-w-7xl mx-auto flex justify-end">
+          <PopoverButton
+            :class="[
+              open ? 'text-gray-900' : 'text-gray-500',
+              'bg-white rounded-md inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-tohl-500'
+            ]"
+          >
+            <CogIcon
+              :class="[
+                open ? 'text-gray-600' : 'text-gray-400',
+                'h-5 w-5 group-hover:text-gray-500'
+              ]"
+              aria-hidden="true"
+            />
+          </PopoverButton>
+        </div>
+
+        <transition
+          enter-active-class="transition ease-out duration-200"
+          enter-from-class="opacity-0 -translate-y-1"
+          enter-to-class="opacity-100 translate-y-0"
+          leave-active-class="transition ease-in duration-150"
+          leave-from-class="opacity-100 translate-y-0"
+          leave-to-class="opacity-0 -translate-y-1"
+        >
+          <PopoverPanel class="absolute z-10 max-w-xl right-0 transform">
+            <div class="bg-white relative border border-gray-300 py-2 px-4 rounded shadow-lg">
+              <h3 class="text-sm font-medium tracking-wide text-gray-500 uppercase">
+                Gruppierungen
+              </h3>
+              <div class="mt-4 border-t border-b border-gray-200 divide-y divide-gray-200">
+                <div
+                  v-for="(group, groupIdx) in groups"
+                  :key="groupIdx"
+                  class="relative flex items-start p-2"
+                >
+                  <div class="min-w-0 flex-1 text-sm">
+                    <label
+                      :for="`group-${group.id}`"
+                      class="font-medium text-gray-700 select-none"
+                      >{{ group.name }}</label
+                    >
+                  </div>
+                  <div class="ml-3 flex items-center h-5">
+                    <input
+                      v-model="group.active"
+                      :id="`group-${group.id}`"
+                      :name="`group-${group.id}`"
+                      type="checkbox"
+                      class="focus:ring-tohl-500 h-4 w-4 text-tohl-600 border-gray-300 rounded"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </PopoverPanel>
+        </transition>
+      </Popover>
+      <Datatable :tabledata="players" :columns="columns" :options="datatableOptions" />
     </div>
     <span class="text-secondary-500 font-bold"></span>
     <span class="text-tohl-500 font-bold underline"></span>
